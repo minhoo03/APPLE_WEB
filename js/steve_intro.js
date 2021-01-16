cc52b17079bbbd75334cc210d200ac62e0dc5406
minhoo03 <minhoo0333@gmail.com>
2021-01-17 00:01:59 +0900

    steve_intro interactive... um....

--- js/steve_intro.js
+++ js/steve_intro.js
@@ -1,10 +1,16 @@
 $(() => {
-    let site_steve = $('.site_steve')
-    let move_img1 = $('.scroll-img1');
+
+    let content_title = document.querySelector('.content_title')
+    let site_steveZoom = document.querySelector('.site_steve')
+    
+    let test200 = $('.test200')
+    let move_img1 = $('.content_box1');
+    let move_img2 = $('.content_box2');
+    let apple_intro = $('.apple_intro')
 
 
-    let height_site_steve // box height
-    let offset_site_steve // box 위치
+    let height_test200 // box height
+    let offset_test200 // box 위치
 
     let scorll_realHeight1 // 내려야 할 스크롤 값
     let win_scrollTop // 현재 스크롤
@@ -17,31 +23,66 @@ $(() => {
     }
 
     function setProperty() {
-        height_site_steve = site_steve.height() // box height
-        offset_site_steve = site_steve.offset().top // box 위치
+        height_test200 = test200.height() // box height
+        offset_test200 = test200.offset().top // box 위치
 
 
         // 보이는 height : $(window).height()
-        scorll_realHeight1 = height_site_steve - $(window).height() // 보이지 않는 height
+        scorll_realHeight1 = height_test200 - $(window).height() // 보이지 않는 height
 
 
         win_scrollTop = $(window).scrollTop() // 현재 스크롤 위치
-        scroll_site_steve = win_scrollTop - offset_site_steve // 현재위치-박스위치
+        scroll_test200 = win_scrollTop - offset_test200 // 현재위치-박스위치
 
         // ex : 0px => 0 - 500px => -500px으로 인식
 
-        scrollPersent1 = scroll_site_steve / scorll_realHeight1
+        scrollPersent1 = scroll_test200 / scorll_realHeight1
 
     }
 
     function motion() {
+        console.log('scrollPersent1',scrollPersent1)
 
+        let moveVal1 = scrollPersent1 * -20
+        let moveVal2 = scrollPersent1 * 100
 
-        let moveVal1 = scrollPersent1 * -10
+        let zoomStart = 1.1
+        let zoomEnd = 1
+        let scrollVal = Math.max(0, scrollPersent1)
+        let zoomVal = Math.max(zoomEnd, zoomStart + (scrollVal * zoomStart * 0.2 ))
+        
+
+        console.log('zoomVal',zoomVal)
 
         move_img1.css({
             'transform': `translate(0px,${moveVal1}px)`
         })
+        move_img2.css({
+            'transform': `translate(0px,${moveVal2}px)`
+        })
+
+        apple_intro.css({
+            'transform': `scale(${zoomVal})`
+        })
+
+        if(scrollPersent1 <= 0.7) {
+            $('.site_steve').addClass('active')
+            console.log('addClass!!!')
+        } else {
+            $('.site_steve').removeClass('active')
+            console.log('removeClass!!')
+        }
+
+        let y = content_title.getBoundingClientRect().top
+
+        // 이미지가 화면의 40%만큼 올라오면 addClass
+        if(y < window.innerHeight * 0.4){
+            site_steveZoom.classList.add('zoom')
+            console.log('addZoom')
+        } else{
+            site_steveZoom.classList.remove('zoom')
+            console.log('removeZoom')
+        }
 
     }
     function init() {
@@ -52,7 +93,7 @@ $(() => {
     function bindEvent() {
         $(window).scroll(function () {
             changeValue()
-            console.log(site_steve.height() - $(window).height());
+            console.log(test200.height() - $(window).height());
         })
 
         $(window).resize(function () {
