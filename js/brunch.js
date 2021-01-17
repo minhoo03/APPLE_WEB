465f3cd47332386ac476b39694f17a1a2f8f2170
minhoo03 <minhoo0333@gmail.com>
2021-01-18 00:15:04 +0900

    getBounding event

--- js/brunch.js
+++ js/brunch.js
@@ -1,10 +1,12 @@
 (() => {
     const siteS = document.querySelector('.site_section')
     const siteP = document.querySelector('.site_product')
+    const max_left = document.querySelector('.max_left')
 
     let scrollTop = 0
     let intro_img = document.getElementsByClassName('intro_img')[0]
     let intro_title = document.getElementsByClassName('intro_title')[0]
+    let max_intro =document.getElementsByClassName('max_intro')[0]
 
     // 변수.getBoundingClientRect()
     // : 변수의 bottom, top, left, right 값을 알 수 있음
@@ -26,9 +28,20 @@
         }
     }
 
+    function showValue3() {
+        let y = max_left.getBoundingClientRect().top
+
+        if(y < window.innerHeight * 0.65) {
+            max_intro.classList.add('active')
+        } else {
+            max_intro.classList.remove('active')
+        }
+    }
+
     // 스크롤시 showValue 실행
     window.addEventListener('scroll', showValue, false)
     window.addEventListener('scroll', showValue2, false)
+    window.addEventListener('scroll', showValue3, false)
     window.addEventListener('scroll', () => {
         scrollTop = document.documentElement.scrollTop
         console.log(scrollTop)
